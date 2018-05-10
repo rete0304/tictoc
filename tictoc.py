6189aae2bfa91175bb43c5748091341f966983a9
Rete <rete0304@gmail.com>
2018-05-10 23:09:01 +0800

    1.增加投降指令 2.增加重開指令 3.增加外迴圈，詢問使用者是否繼續玩

--- tictoc.py
+++ tictoc.py
@@ -16,6 +16,22 @@ def app():
         #2.X贏
         #3.格子填完但是沒有任何一人獲勝
         #(3也可以簡化成雙方下了9次但是沒有分出勝負)
-
+    del gameObj
 if __name__ == "__main__":
-    app()
+    isPlay = True
+    while(isPlay == True):
+        app()
+        isContinue = True
+        while isContinue == True:
+            print "Are you went to paly the game(y/n):"
+            inp = raw_input()
+            if inp == "n":
+                print "Good Bye!!"
+                isPlay = False
+                isContinue = False
+            elif inp == "y":
+                print "Let's Rock!!"
+                isPlay = True
+                isContinue = False
+            else:
+                print "Enter Wrong Answer"
